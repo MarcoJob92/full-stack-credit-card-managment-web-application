7cd659cab09a5c43828f6e66d4a310422e722459
Marco Furone <marco.job92@gmail.com>
2020-05-11 13:08:38 +0100

    Update CreditCardServiceIntegrationTest.java

--- backend_credit_card_system_marco_furone_publicis_sapient/src/test/java/com/fullstackwebbapp/maven/backend_credit_card_system_marco_furone_publicis_sapient/CreditCardServiceIntegrationTest.java
+++ backend_credit_card_system_marco_furone_publicis_sapient/src/test/java/com/fullstackwebbapp/maven/backend_credit_card_system_marco_furone_publicis_sapient/CreditCardServiceIntegrationTest.java
@@ -1,53 +1,63 @@
 package com.fullstackwebbapp.maven.backend_credit_card_system_marco_furone_publicis_sapient;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.dao.DuplicateKeyException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-public class CreditCardServiceIntegrationTest {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fullstackwebbapp.maven.backend_credit_card_system.entities.CreditCard;
+import com.fullstackwebbapp.maven.backend_credit_card_system_marco_furone_publicis_sapient.controllers.CreditCardController;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import org.assertj.core.util.Lists;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestConfig.class})
+
+public class CreditCardServiceIntegrationTests {
 	
-	CreditCardService creditCardManagementService;
+	@Autowired
+	CreditCardController controller;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void init() {
+        mockMvc = standaloneSetup(controller).build();
+    }
+    
+    
+    @Test
+    public void testGetAllCreditCardsIsEmpty_testPostServiceToAddCreditCardObjects_testGetAllCreditCardsIsCorrectlyPopulated() throws Exception {
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/creditCards/getAll"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+        .andExpect(MockMvcResultMatchers.jsonPath("$").isEmpty());
+        
+        mockMvc.perform( MockMvcRequestBuilders
+            	.post("/creditCards/add")
+                .content(new ObjectMapper().writeValueAsString(new CreditCard("Marco", "1111222233334444", 20, 0)))
+                .contentType(MediaType.APPLICATION_JSON)
+    		    .accept(MediaType.APPLICATION_JSON))
+    		    .andExpect(status().isOk());
+        
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/creditCards/getAll"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[*].number").value(Lists.newArrayList("1111222233334444")));
+    }
 	
-	@Before
-	public void init() {
-		creditCardManagementService = new CreditCardService();
-		CreditCardService.init();
-	}
-
-
-	@Test
-	public void addCrediCard_retriveCard_testDupliacteKeys() {
-		String cardNumber = "3333444422221111";
-		CreditCard cd = new CreditCard("Marco", cardNumber, 1000, 0);
-		creditCardManagementService.addCreditCard(cd);
-		
-		CreditCard found = creditCardManagementService.findAll()
-				.stream()
-				.filter(card -> card.getNumber().equals(cardNumber))
-				.findFirst()
-				.get();
-		
-		assertTrue( found.getNumber().equals(cd.getNumber()) );
-		
-		try {
-			creditCardManagementService.addCreditCard(cd);
-			fail();
-		} catch (DuplicateKeyException e) {}
-	}
-	
-	@Test
-	public void testLuhn10Algorithm() {
-		String cardNumber = "4716-4359-1733-0099";
-		CreditCard cd = new CreditCard("Marco", cardNumber, 100, 0);
-		assertTrue( cd.checkLuhn10() );
-		
-		String wrongCardNumber = "1111-2222-1111-2222";
-		CreditCard cd2 = new CreditCard("Marco", wrongCardNumber, 100, 0);
-		assertFalse( cd2.checkLuhn10() );
-	}
-
 }
